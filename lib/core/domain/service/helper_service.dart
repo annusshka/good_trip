da3cdb64e03e05e0a795756cf9e75ff2cc48e10e
annusshka <telegina.ann2003@yandex.ru>
2024-03-25 22:08:36 +0300

    add helper service

---
+++ lib/core/domain/service/helper_service.dart
@@ -0,0 +1,26 @@
+class HelperService {
+  static const String host = "localhost";
+  static const int port = 5432;
+  static const String scheme = "http";
+  static const String apiPath = "/api/";
+
+  static Uri buildUri(String path) {
+    return Uri(
+      scheme: scheme,
+      host: host,
+      port: port,
+      path: apiPath + path,
+    );
+  }
+
+  static Map<String, String> buildHeaders({String? accessToken}) {
+    Map<String, String> headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    };
+    if (accessToken != null) {
+      headers['Authorization'] = 'Bearer $accessToken';
+    }
+    return headers;
+  }
+}
