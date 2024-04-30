7f353d3d44bff2f98f5b42fdcdec45c2bd09705e
annusshka <alexandra.wolk@yandex.ru>
2024-05-01 00:41:30 +0300

    update theme

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -5,6 +5,7 @@ class AppColors {
   final white = const Color(0xFFFFFFFF);
   final black = const Color(0x00000000);
   final lightGrayNavBar = const Color(0xFFCDD2DF);
+
   final lightGray1 = const Color.fromARGB(100, 241, 241, 245);
   final lightGray2 = const Color.fromARGB(100, 226, 226, 234);
   final lightGray = const Color.fromARGB(100, 181, 181, 190);
@@ -12,6 +13,16 @@ class AppColors {
   final darkGray2 = const Color.fromARGB(100, 0, 0, 20);
   final darkGray = const Color.fromARGB(100, 23, 23, 37);
   final pink_ = const Color.fromARGB(255, 255, 158, 162);
+
+  /*
+  final lightGray1 = const Color.fromRGBO(241, 241, 245, 1);
+  final lightGray2 = const Color.fromRGBO(226, 226, 234, 1);
+  final lightGray = const Color.fromRGBO(181, 181, 190, 1);
+  final gray = const Color.fromRGBO(105, 105, 116, 1);
+  final darkGray2 = const Color.fromRGBO(0, 0, 20, 1);
+  final darkGray = const Color.fromRGBO(23, 23, 37, 1);
+  final pink_ = const Color.fromARGB(255, 255, 158, 162);
+   */
 
   const AppColors();
 }
---
+++ lib/core/theme/strings.dart
@@ -0,0 +1,18 @@
+const String goodTrip = 'Good Trip';
+
+const String registration = 'Регистрация';
+const String authorization = 'Авторизация';
+const String register = "Зарегистрироваться";
+const String registerBotanic = "Зарегистрировать ботаника";
+const String authorize = 'Авторизоваться';
+const String logOut = 'Выйти';
+const String or = "или";
+const String enterWithoutAuth = "Войти без регистрации";
+const String haveAccountQuestion = "Ещё нет аккаунта? ";
+const String alreadyHaveAccount = "Уже есть аккаунт? ";
+
+const String enterLogin = 'Введите логин';
+const String enterPassword = 'Введите пароль';
+const String enterConfirmPassword = 'Повторите пароль';
+const String enterMin6 = 'Введите минимум 6 символов';
+const String passwordsDontMatch = "Пароли не совпадают";
