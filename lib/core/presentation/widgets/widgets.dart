dbd4f6b8edfc5eeadcb9fb01fa9d09698cd49539
annusshka <alexandra.wolk@yandex.ru>
2024-05-21 11:07:53 +0300

    update base settings

--- lib/core/presentation/widgets/widgets.dart
+++ lib/core/presentation/widgets/widgets.dart
@@ -1,9 +1,11 @@
+export 'empty_list.dart';
 export 'search_delegate.dart';
+export 'tour_create_list_element.dart';
 export 'tour_grid.dart';
 export 'tour_scroll_element.dart';
 export 'tour_scroll_list.dart';
 export 'nav_bar/nav_bar_selected_icon.dart';
-export 'nav_bar/app_navigation_screen.dart';
+export 'nav_bar/nav_bar_user_screen.dart';
 export 'nav_bar/nav_bar_element.dart';
 export 'logo.dart';
 export 'buttons/buttons.dart';
